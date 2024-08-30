a463749e50021aed70875da6f9e9dcc993974184
Hammad Ahmed <ha92az@gmail.com>
2024-08-30 07:15:29 -0700

    Add remaining files and directories

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express")
+const path = require("path")
+const app = express()
+const fs = require('fs')
+
+
+
+app.set('view engine', 'ejs');
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+
+app.use(express.static(path.join(__dirname ,"public")))
+
+app.get('/' , (req , res)=>
+    {
+
+    fs.readdir(`./files`, function(err  , files)
+    {
+        console.log(files)
+   
+    res.render('index',{files:files})
+    });
+} )
+
+app.post('/create' , (req , res)=>
+{
+    fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt` , req.body.details , function(err)
+{
+    res.redirect("/")
+}) 
+
+} )
+
+app.listen(3000);
